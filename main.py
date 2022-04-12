cc06521b95c0b3945299439302f58bac39194419
woqq111 <2010826@mail.nankai.edu.cn>
2022-04-12 13:30:33 +0800

    1

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from util import load_data, in_school, out_school
 
 if __name__ == '__main__':
     req_body, req_type = load_data()
-    cookie_str = os.environ['PHPSESSID']
+    cookie_str = "PHPSESSID=gfjm36hh3lcchtrm7kl09ia7s3"
+    # cookie_str = "gfjm36hh3lcchtrm7kl09ia7s3"
+    # cookie_str = os.environ['PHPSESSID']
     if req_type == 'inschool':
         res = in_school(req_body, cookie_str)
     else:
